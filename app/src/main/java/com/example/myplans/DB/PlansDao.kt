af1cbb37c5cbc5a4862af1d56c7bd559e600159e
areej <areej101223@gmail.com>
2022-03-02 15:11:22 +0300

    add Repository

--- app/src/main/java/com/example/myplans/DB/PlansDao.kt
+++ app/src/main/java/com/example/myplans/DB/PlansDao.kt
@@ -1,19 +1,20 @@
 package com.example.myplans.DB
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.example.myplans.DB.Relations.SemesterWithCourses
 
 @Dao
 interface PlansDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertSemester(Semester: Semester): Long
+    fun insertSemester(Semester: Semester): Long
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertCourse(course: Course): Long
+    fun insertCourse(course: Course): Long
 
 
     @Transaction
     @Query("SELECT * FROM semester WHERE semester = :Semester")
-    suspend fun getSemesterWithCourse(Semester: String): List<SemesterWithCourses>
+    fun getSemesterWithCourse(Semester: String): LiveData<List<SemesterWithCourses>>
 
 }
---
+++ app/src/main/java/com/example/myplans/DB/Repository.kt
@@ -0,0 +1,16 @@
+package com.example.myplans.DB
+
+import androidx.lifecycle.LiveData
+import com.example.myplans.DB.Relations.SemesterWithCourses
+
+class Repository(private val planDoa: PlansDao, semesterName: String) {
+    val getClass: LiveData<List<SemesterWithCourses>> = planDoa.getSemesterWithCourse(semesterName)
+
+    suspend fun insertSemester(newSemester: Semester) {
+        planDoa.insertSemester(newSemester)
+    }
+
+    suspend fun insertCourse(newCourse: Course) {
+        planDoa.insertCourse(newCourse)
+    }
+}
